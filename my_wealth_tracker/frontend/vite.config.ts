0e14eb38e2108240ec062a8c838af96516dedb9e
Oisin Conolly <oisin@oacdesigns.com>
2023-11-21 15:51:01 +0000

    chore: add docker build step for frontend
    - adds a build stage in the Dockerfile to build the frontend, and then
      copy the build artifacts to a public folder in the frontend service
    - adds a frontend service that sets up a simple pythong server serving
      the frontend assets from a public folder
    - updates the Vite.config.ts to ensure relative urls are used, this is
      to work properly with the HA ingress (which uses an iframe)

--- my_wealth_tracker/frontend/vite.config.ts
+++ my_wealth_tracker/frontend/vite.config.ts
@@ -5,9 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: '', // focrce relative paths, to work with Homeassistant ingress
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
