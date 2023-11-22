e2056354e8258a161221c3d75267ce86f60add00
Oisin Conolly <oisin@oacdesigns.com>
2023-11-22 09:53:25 +0000

    feat: add tailwind css for styling
    - installs and sets up tailwind css for styling.
    - makes small modifications to a component to test that it works
    - adds vscode extentions to devcontainer for Vue and tailwind

--- my_wealth_tracker/frontend/src/components/HelloWorld.vue
+++ my_wealth_tracker/frontend/src/components/HelloWorld.vue
@@ -6,8 +6,8 @@ defineProps<{
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
+    <h1 class="text-5xl no-underline text-green-600">{{ msg }}</h1>
+    <h3 class="text-lg">
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
@@ -16,22 +16,6 @@ defineProps<{
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
 @media (min-width: 1024px) {
   .greetings h1,
   .greetings h3 {
